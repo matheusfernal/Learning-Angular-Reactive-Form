1475d8e3d1e97484d3cb52730d6864d86174c3e3
Matheus Andrade Fernal <MatheusAF@tcerj.intra>
2017-11-09 18:22:13 -0200

    Adding new fields to the form

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+import { states } from './data-model';
+
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html'
 })
 export class HeroDetailComponent {
   heroForm: FormGroup;
+  states = states;
 
   constructor(private formBuilder: FormBuilder)  {
     this.createForm();
@@ -15,6 +18,12 @@ export class HeroDetailComponent {
   createForm(): void {
     this.heroForm = this.formBuilder.group({
       name: ['', Validators.required],
+      street: '',
+      city: '',
+      statate: '',
+      zip: '',
+      power: '',
+      sidekick: ''
     });
   }
 }
